289d624bb3ff449fff22aa6546f9318844eecfa6
aaccurso <accurso.alan@gmail.com>
2018-08-28 23:24:27 -0300

    Add graphql REPL

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const repl = require('repl');
 const { graphql, buildSchema } = require('graphql');
 
+// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
+    hello(name: String): String!
   }
 `);
 
-const root = { hello: () => 'Hello world!' };
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello: ({ name }) => `Hello ${name || 'World'}`
+};
 
-graphql(schema, '{ hello }', root).then(console.log);
+// Eval REPL query input and print out the response
+const graphqlEval = (query, context, filename, callback) => {
+    graphql(schema, query, root).then(response => callback(null, response));
+};
+
+repl.start({ prompt: '> ', eval: graphqlEval });
