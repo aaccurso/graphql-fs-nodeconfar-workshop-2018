47559061e4bf4b6a0b03fee788f8236d9febd8a0
aaccurso <accurso.alan@gmail.com>
2018-09-02 23:37:05 -0300

    Add readDir resolver

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const repl = require('repl');
 const { graphql, buildSchema } = require('graphql');
+const fs = require('fs');
+const util = require('util');
+
+const readDir = util.promisify(fs.readdir);
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello(name: String): String!
+    readDir(dir: String): [String!]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: ({ name }) => `Hello ${name || 'World'}`,
+  readDir: ({ dir }) => readDir(`__tests__/${dir || ''}`),
 };
 
 // Eval REPL query input and print out the response
