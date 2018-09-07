d18ea811003f01c33b801960583d10564c57e625
aaccurso <accurso.alan@gmail.com>
2018-09-06 23:18:30 -0300

    Add File and Dir types

--- src/index.js
+++ src/index.js
@@ -3,62 +3,95 @@ const { graphql, buildSchema } = require('graphql');
 const fs = require('fs');
 const util = require('util');
 
-const readDir = util.promisify(fs.readdir);
-const writeFile = util.promisify(fs.writeFile);
-const statFn = util.promisify(fs.stat);
+const fsReadDir = util.promisify(fs.readdir);
+const fsWriteFile = util.promisify(fs.writeFile);
+const fsStat = util.promisify(fs.stat);
+
+const ROOT_PATH = '__tests__/mockDir';
+const FILE_TYPE = 'File';
+const DIR_TYPE = 'Dir';
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello(name: String): String!
-    readDir(dir: String): [Stat!]!
+    files(dir: String): [File!]!
+    dirs(dir: String): [Dir!]!
   }
   type Mutation {
-    writeFile(name: String!, content: String!): Stat!
+    writeFile(name: String!, content: String!): File!
   }
-  type Stat {
+  type File {
     name: String!
     type: String!
   }
+  type Dir {
+    name: String!
+    type: String!
+    parent: String!
+    files(dir: String): [File!]!
+    dirs(dir: String): [Dir!]!
+  }
 `);
 
-class Stat {
-  constructor({ name, type }) {
+let readDir;
+
+class File {
+  constructor({ name }) {
     this.name = name;
-    this.type = type;
+    this.type = FILE_TYPE;
   }
 }
 
-const mockDirPath = '__tests__/mockDir';
+class Dir {
+  constructor({ name, parent }) {
+    this.name = name;
+    this.parent = parent;
+    this.type = DIR_TYPE;
+  }
 
-const FILE_TYPE = 'file';
-const DIR_TYPE = 'dir';
+  files() {
+    return readDir({
+      dir: this.name,
+      parent: this.parent,
+      typeFilter: FILE_TYPE,
+    });
+  }
+
+  dirs() {
+    return readDir({
+      dir: this.name,
+      parent: this.parent,
+      typeFilter: DIR_TYPE,
+    });
+  }
+}
+
+readDir = async ({ dir, parent, typeFilter }) => {
+  const parentPath = parent ? `${parent}/` : '';
+  const path = `${ROOT_PATH}/${parentPath}${dir || ''}`;
+  const files = await fsReadDir(path);
+
+  const stats = await Promise.all(files.map(async (name) => {
+    const stat = await fsStat(`${path}/${name}`);
+    return stat.isFile()
+      ? new File({ name })
+      : new Dir({ name, parent: `${parentPath}${dir || ''}` });
+  }));
+
+  return stats.filter(({ type }) => type === typeFilter);
+};
 
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: ({ name }) => `Hello ${name || 'World'}`,
-  readDir: async ({ dir }) => {
-    const path = `${mockDirPath}/${dir || ''}`;
-    const files = await readDir(path);
-
-    const stats = await Promise.all(files.map(async (name) => {
-      const stat = await statFn(`${path}/${name}`);
-      const type = stat.isFile() ? FILE_TYPE : DIR_TYPE;
-
-      return new Stat({
-        name,
-        type,
-      });
-    }));
-
-    return stats;
-  },
+  files: args => readDir({ ...args, typeFilter: FILE_TYPE }),
+  dirs: args => readDir({ ...args, typeFilter: DIR_TYPE }),
   writeFile: async ({ name, content }) => {
-    await writeFile(`${mockDirPath}/${name}`, content);
+    await fsWriteFile(`${ROOT_PATH}/${name}`, content);
 
-    return new Stat({
+    return new File({
       name,
-      type: FILE_TYPE,
     });
   },
 };
